a7a0e3da4887581b3295ea1f98bf6c9f9e159f87
egyou <egyou@induk.ac.kr>
2023-04-28 15:12:46 +0900

    First Commit

---
+++ src/main/java/idusw/springboot/SwBoardApplication.java
@@ -0,0 +1,13 @@
+package idusw.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwBoardApplication.class, args);
+    }
+
+}
